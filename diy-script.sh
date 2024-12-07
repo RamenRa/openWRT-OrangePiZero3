9788c897fe042e03153800db60ac804826986dec
RamenRa <53000259+RamenRa@users.noreply.github.com>
2024-12-07 23:23:18 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -3,6 +3,19 @@
 # 修改默认IP
 # sed -i 's/192.168.113.8/192.168.1.1/g' package/base-files/files/bin/config_generate
 
+# 修改 LAN 接口为 DHCP 获取 IP 地址
+sed -i 's/option proto static/option proto dhcp/g' package/base-files/files/etc/config/network
+
+# 启用 LAN 接口的 DHCP 服务器
+cat >> package/base-files/files/etc/config/dhcp <<EOF
+config dhcp 'lan'
+    option interface 'lan'
+    option start '100'
+    option limit '150'
+    option leasetime '12h'
+    option ignore '0'
+EOF
+
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
